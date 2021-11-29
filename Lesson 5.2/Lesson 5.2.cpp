7bd5cc032a4e124270596db86f0a37d577100d75
DivergenOko <77746850+DivergenOko@users.noreply.github.com>
2021-11-29 15:30:59 +0300

    Add files via upload

---
+++ Lesson 5.2/Lesson 5.2.cpp
@@ -0,0 +1,22 @@
+﻿// Задача №8. Оператор вводит n - количество чисел с экрана, вводит n чисел. 
+// Посчитать сумму и среднее арифметическое всех числе.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, N, i = 1, sum = 0;
+    float sr;
+    cout << "Number: ";
+    cin >> num;
+
+    for (i ;i <= num; i++) {
+        cout << "N" << i << ": ";
+        cin >> N;
+        sum += N;
+        cout << "sum = " << sum << endl;
+    }
+    sr = float(sum / num);
+    cout << "sr = " << sr << endl;
+}
