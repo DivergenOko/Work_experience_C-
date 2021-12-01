e385b77b6b8bae05628d08046c8f24ab0fcbc465
DivergenOko <77746850+DivergenOko@users.noreply.github.com>
2021-12-02 01:35:34 +0300

    Add files via upload

---
+++ Lesson 7.2/Lesson 7.2.cpp
@@ -0,0 +1,27 @@
+﻿// Lesson 7.2: Заполнение массива случайными ВЕЩЕСТВЕННЫМИ числами и вывод на экран.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+	float min, max;
+	int n;
+	cout << "Input n in massiv: ";
+	cin >> n;
+	cout << endl << "Input diapason min: ";
+	cin >> min;
+	cout << endl << "Input diapason max: ";
+	cin >> max;
+	vector<float> A(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		A[i] = rand() * (max - min) / RAND_MAX + min;
+		cout << i << "--> ";
+		cout << "[" << A[i] << "]" << " " << endl;
+	}
+
+}
