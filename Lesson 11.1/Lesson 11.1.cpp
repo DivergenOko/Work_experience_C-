d108ad58377ab69e56e3f931a28261bc720230fc
Yana_Senchenko <77746850+DivergenOko@users.noreply.github.com>
2022-03-17 19:10:46 +0300

    Add files via upload

---
+++ Lesson 11.1/Lesson 11.1.cpp
@@ -0,0 +1,37 @@
+﻿// Lesson 11.1: Статический массив.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	const int MaxSize = 100;
+	int Mass1[MaxSize];
+	int Mass2[MaxSize];
+
+	// int Mass3[] = {2, 5, 7, 1, 0};
+
+	int N, M = 0;
+	cin >> N;
+	if (N < 0 || N > MaxSize)
+		cout << "Size not correct!";
+	else
+	{
+		for (int i = 0; i < N; i++)
+			cin >> Mass1[i];
+
+		for (int i = 0; i < N; i++)
+			if (Mass1[i] % 3 == 0)
+			{
+				Mass2[M] = Mass1[i];
+				M++;
+			}
+
+		if (M > 0)
+			for (int i = 0; i < M; i++)
+				cout << Mass2[i] << " ";
+		else cout << "Massiv is empty!" << "\n";
+	}
+	return 0;
+}
---
+++ Lesson 11.2/Lesson 11.2.cpp
@@ -0,0 +1,35 @@
+﻿// Lesson 11.2 : заполнить массив с клавиатуры;  
+// и высети те элементы которые делятся на 3;  
+// выводить элементы которые делятся на 3 без остатка в отдельный массив.
+
+#include <iostream> 
+#include <vector> 
+using namespace std;
+
+int main()
+{
+    int n, k = 0, j = 0;
+    cin >> n;
+    vector<int> x = vector<int>(n);
+    vector<int> y = vector<int>(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (x[i] % 3 == 0) {
+            y[k] = x[i];
+            k++;
+        }
+    }
+    y.resize(k);
+    cout << k << endl << n;
+    if (y.size() > 0) {
+        for (j; j < k; j++) {
+            cout << "y[" << y[j] << "]; ";
+        }
+    }
+    else {
+        cout << "y is emty!";
+    }
+}
