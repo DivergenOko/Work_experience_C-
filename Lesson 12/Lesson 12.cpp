4ba9f64283898a9816feedf7945867ebf6ee589b
Yana_Senchenko <77746850+DivergenOko@users.noreply.github.com>
2022-03-21 19:49:33 +0300

    Add files via upload

---
+++ Lesson 12/Lesson 12.cpp
@@ -0,0 +1,18 @@
+﻿// Lesson 12
+
+#include <iostream> 
+#include <vector> 
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int* M = &x;
+    *M = 20; // х будет равен 20
+    long long int z = 0xFF11F11F001FF1;
+
+    unsigned char* K = (unsigned char*)&z; //преобразуем в другой тип данных
+    for (int i = 0; i < 8; i++) {
+        cout << int(K[i]) << " ";
+    }
+}
