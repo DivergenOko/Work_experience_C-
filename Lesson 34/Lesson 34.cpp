3d22cc2ddc66194abfca1c8b0e66b797eac53130
Yana_Senchenko <77746850+DivergenOko@users.noreply.github.com>
2022-03-30 16:19:01 +0300

    Add files via upload

---
+++ Lesson 34/Lesson 34.cpp
@@ -0,0 +1,76 @@
+﻿// Lesson 34. Поменять местами столбцы и строки 
+// Отладка: 
+// n = 3, m = 5 
+// Ввод: 
+// 1 2 3 4 5  
+// 6 7 8 9 10 
+// 11 12 13 14 15 
+// Вывод: 
+// 1 6 11 
+// 2 7 12 
+// 3 8 13 
+// 4 9 14 
+// 5 10 15 
+
+#include <iostream>  
+using namespace std;
+
+int main()
+{
+    int n, m; // n - строки, m - столбцы  
+    cout << "n = "; cin >> n;
+    cout << "m = "; cin >> m;
+    // Инициализация массивов 
+    int** matrix = new int* [n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[m]; // для нной строки создаем м столбцов 
+    }
+
+    int** matrix_copy = new int* [m];
+    for (int i = 0; i < m; i++) {
+        matrix_copy[i] = new int[n];
+    }
+
+    // Заполнение первоначального массива 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matrix[i][j] = rand() % 11;
+        }
+    }
+
+    // Вывод на экран 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+
+    cout << "___________________________" << endl;
+
+    // Копируем элементы первого массива во второй 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matrix_copy[j][i] = matrix[i][j];
+        }
+    }
+
+    // у второго массива теперь количество столбцов равно количеству строк 
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix_copy[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+
+    // Очистка памяти 
+    for (int i = 0; i < n; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    for (int i = 0; i < n; i++) {
+        delete[] matrix_copy[i];
+    }
+    delete[] matrix_copy;
+}
---
+++ Lesson 35/Lesson 35.cpp
@@ -0,0 +1,67 @@
+﻿// Lesson 35. Сумма двух матриц
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int Rows, Cols;
+	cin >> Rows >> Cols;
+
+	int** Mass1 = new int* [Rows];
+	for (int i = 0; i < Rows; i++) {
+		Mass1[i] = new int[Cols];
+	}
+		
+	int** Mass2 = new int* [Rows];
+	for (int i = 0; i < Rows; i++) {
+		Mass2[i] = new int[Cols];
+	}
+		
+	int** Mass3 = new int* [Rows];
+	for (int i = 0; i < Rows; i++) {
+		Mass3[i] = new int[Cols];
+	}
+		
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Cols; j++) {
+			cin >> Mass1[i][j];
+		}
+	}
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Cols; j++) {
+			cin >> Mass2[i][j];
+		}
+	}
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Cols; j++) {
+			Mass3[i][j] = Mass1[i][j] + Mass2[i][j];
+		}
+	}
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Cols; j++) {
+			cout << Mass3[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < Rows; i++) {
+		delete[] Mass1[i];
+	}
+	delete[] Mass1;
+
+	for (int i = 0; i < Rows; i++) {
+		delete[] Mass2[i];
+	}
+	delete[] Mass2;
+
+	for (int i = 0; i < Rows; i++) {
+		delete[] Mass3[i];
+	}
+	delete[] Mass3;
+
+	return 0;
+}
