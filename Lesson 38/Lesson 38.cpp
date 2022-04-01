627aeaad867aaab7033d4219e0688d294c189e5a
Yana_Senchenko <77746850+DivergenOko@users.noreply.github.com>
2022-04-01 20:23:40 +0300

    Add files via upload

---
+++ Lesson 38/Lesson 38.cpp
@@ -0,0 +1,99 @@
+﻿// Lesson 38. сложение двух массивов, заполнение из файла.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	fstream Input = fstream("Input.txt", ios::in);
+	fstream Output = fstream("Output.txt", ios::out);
+	int n, m;
+	int** mas1 = 0;
+	int** mas2 = 0;
+
+	if (Input.good()) {
+
+		Input >> n >> m;
+		mas1 = new int* [n];
+		mas2 = new int* [n];
+
+
+		int index1 = 0;
+		for (int i = 0; i < n; i++) {
+			mas1[index1] = new int[m];
+			for (int j = 0; j < m; j++) {
+				Input >> mas1[i][j];
+			}
+			index1++;
+		}
+
+
+		int index2 = 0;
+		for (int i = 0; i < n; i++) {
+			mas2[index2] = new int[m];
+			for (int j = 0; j < m; j++) {
+				Input >> mas2[i][j];
+			}
+			index2++;
+		}
+		Input.close();
+	}
+
+	if (Output.good()) {
+
+		if (mas1 != 0 && mas2 != 0) {
+			int** mas3 = new int* [n];
+			int index3 = 0;
+			for (int i = 0; i < n; i++) {
+				mas3[index3] = new int[m];
+				for (int j = 0; j < m; j++) {
+					mas3[i][j] = mas1[i][j] + mas2[i][j];
+				}
+				index3++;
+			}
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) cout << mas3[i][j] << " ";
+				cout << "\n";
+			}
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) Output << mas3[i][j] << " ";
+				Output << "\n";
+			}
+
+			//for (int i = 0; i < n; i++) delete[] mas3[i];
+			//delete[] mas3;
+
+			Output.close();
+			fstream BinaryFile = fstream("Output.bin", ios::out);
+			if (BinaryFile.good()) {
+				// функция райт записывает в файл бинарные значения
+				BinaryFile.write((char*)&n, sizeof(n));//в файл выводится в бинарном виде кол-во строк
+				BinaryFile.write((char*)&m, sizeof(m));// в файл выводится кол-во столбцов в бинарном виде
+				for (int i = 0; i < n; i++) {
+					BinaryFile.write((char*)mas3[i], sizeof(mas3[0][0]) * m);//выыодится i-ая строка в бинарном виде
+				}
+				BinaryFile.close();
+			}
+
+			//чтение из бинарного файла и вывод на экран
+			BinaryFile.open("Output.bin");
+			if (BinaryFile.good()) {
+				BinaryFile.read((char*)&n, sizeof(n));
+				BinaryFile.read((char*)&m, sizeof(m));
+				for (int i = 0; i < n; i++)
+					BinaryFile.read((char*)mas3[i], sizeof(mas3[0][0]) * m);
+				for (int i = 0; i < n; i++) cout << mas3[i] << " ";
+			}
+			for (int i = 0; i < n; i++) delete[] mas3[i];
+			delete[] mas3;
+		}
+	}
+
+	//for (int i = 0; i < n; i++) delete[] mas1[i];
+	//for (int i = 0; i < n; i++) delete[] mas2[i];
+
+	//delete[] mas1;
+	//delete[] mas2;
+}
---
+++ Lesson 39/Lesson 39.cpp
@@ -0,0 +1,156 @@
+﻿// Lesson 39. Задача: заполнить два одинаковых по размеру массива (не квадрат)
+// из файла и сделать массив состоящий из чередующихся строк первого массива и второго 
+// и массив состоящийиз чередующихся столбцов и вывести в два разных файла
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int n = 0, m = 0; // n - строки, m - столбцы
+    int** arr1 = 0;
+    int** arr2 = 0;
+    int** arr_str = 0; // массив из строк
+    int** arr_slb = 0; // массив из столбцов
+    fstream input = fstream("Input.txt", ios::in); // файл для ввода обоих массивов
+    fstream output1 = fstream("Output1.txt", ios::out); // файл для вывода массива из строк
+    fstream output2 = fstream("Output2.txt", ios::out); // файл для вывода массива из столбцов
+
+    if (input.good()) { // проверка наличия файла и готовности к потоку данных
+        input >> n >> m;
+        // Инициализация массивов
+        int** arr1 = new int* [n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = new int[m]; // для n-ой строки создаем m столбцов 
+        }
+
+        int** arr2 = new int* [n];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = new int[m];
+        }
+
+        int** arr_str = new int* [n * 2];
+        for (int i = 0; i < n * 2; i++) {
+            arr_str[i] = new int[m];
+        }
+
+        int** arr_slb = new int* [n];
+        for (int i = 0; i < n; i++) {
+            arr_slb[i] = new int[m * 2];
+        }
+
+        // Заполнение массивов из файла
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                input >> arr1[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                input >> arr2[i][j];
+            }
+        }
+
+        // Вывод на экран заполненных массивов (для отладки)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cout << arr1[i][j] << "\t";
+            }
+            cout << "\n";
+        }
+        cout << "------------------------------------------" << endl;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cout << arr2[i][j] << "\t";
+            }
+            cout << "\n";
+        }
+        cout << "------------------------------------------" << endl;
+
+        // Массив с чередующимися строками
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr_str[i * 2][j] = arr1[i][j];
+                arr_str[i * 2 + 1][j] = arr2[i][j];
+            }
+        }
+
+        // Вывод массива с чередующимися строками
+        for (int i = 0; i < n * 2; i++) {
+            for (int j = 0; j < m; j++) {
+                cout << arr_str[i][j] << "\t";
+            }
+            cout << "\n";
+        }
+        cout << "------------------------------------------" << endl;
+
+        // Массив с чередующимися столбцами
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                arr_slb[i][j * 2] = arr1[i][j];
+                arr_slb[i][j * 2 + 1] = arr2[i][j];
+            }
+        }
+
+        // Вывод массива с чередующимися столбцами
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m * 2; i++) {
+                cout << arr_slb[j][i] << "\t";
+            }
+            cout << "\n";
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////
+                // Открытие файла для записи массива из строк
+        if (output1.good()) {
+            for (int i = 0; i < n * 2; i++) {
+                for (int j = 0; j < m; j++) {
+                    output1 << arr_str[i][j] << "\t";
+                }
+                cout << "\n";
+            }
+            output1.close();
+        }
+
+        // Открытие файла для записи массива из столбцов
+        if (output2.good()) {
+            for (int j = 0; j < n; j++) {
+                for (int i = 0; i < m * 2; i++) {
+                    output2 << arr_slb[j][i] << "\t";
+                }
+                cout << "\n";
+            }
+            output2.close();
+        }
+        input.close(); // закрытие файла
+    }
+
+    /* Очистка памяти
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            delete[] arr1[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            delete[] arr2[i][j];
+        }
+    }
+    for (int i = 0; i < n * 2; i++) {
+        for (int j = 0; j < m; j++) {
+            delete[] arr_str[i][j];
+        }
+    }
+
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m * 2; i++) {
+            delete[] arr_slb[j][i];
+        }
+    }*/
+
+    return 0;
+}
+
